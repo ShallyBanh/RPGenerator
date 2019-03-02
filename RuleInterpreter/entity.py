2f86f20b2a162985a6b40b5d0552cf7f1d2b446c
ShallyBanh <banh.shally@gmail.com>
2019-03-02 14:43:27 -0700

    started entity class

---
+++ RuleInterpreter/entity.py
@@ -0,0 +1,16 @@
+class Entity:
+    """
+    Entity Class
+
+    args:
+        None for now
+    """
+
+    def __init__(self):
+        self._type = None
+        self._name = None
+        self._id = None
+        self._actions = None 
+        self._attributes = None 
+        self._size = None 
+        self._last_action = None
