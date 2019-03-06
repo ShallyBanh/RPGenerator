5522231062743d08915bf966db7a871e1ad991cb
Bianca Angotti <bangotti@ualberta.ca>
2019-03-06 15:48:28 -0700

    created and tested basics of tileset
    add basic test images, refactor map, basics tileset test to utilize for game map

--- game_engine/map.py
+++ game_engine/map.py
@@ -3,21 +3,35 @@ class Map:
     """
     Map Class
     args:
-        None for now
+        tilesize, width, height
     """
 
-    def __init__(self, width, height):
-        self._size = (width, height)
-        self._tileset = None
+    def __init__(self, tilesize, width, height):
+        self.width = width
+        self.height = height
+        self.tilesize = tilesize
+        self._tileset = [[0 for w in range(width)] for h in range(height)]
 
-    def get_size(self):
-        return self._size
+    # def get_width(self):
+    #     return self._width
 
-    def set_size(self, width, height):
-        self._size = (width, height)
+    # def set_width(self, width):
+    #     self._width = width
+
+    # def get_height(self):
+    #     return self._height
+
+    # def set_height(self, height):
+    #     self._height = height
+
+    # def get_tilesize(self):
+    #     return self._tilesize
+
+    # def set_tilesize(self, tilesize):
+    #     self._tilesize = tilesize
 
     def get_tileset(self):
-        return _tileset
+        return self._tileset
 
     def set_tileset(self, tileset):
         self._tileset = tileset
---
+++ tilesetbasics.py
@@ -0,0 +1,43 @@
+import pygame, sys, random
+from pygame.locals import *
+from game_engine.map import Map
+
+# sources for examples:
+# http://usingpython.com/pygame-tilemaps/
+# http://usingpython.com/list-comprehension/
+
+myMap = Map(tilesize = 40, height = 15, width = 25)
+
+textures = {
+	"ROCK": pygame.transform.scale(pygame.image.load("images/rock.png"), (myMap.tilesize,myMap.tilesize)),
+	"GRASS": pygame.transform.scale(pygame.image.load("images/grass.png"), (myMap.tilesize,myMap.tilesize)),
+	"WATER": pygame.transform.scale(pygame.image.load("images/water.png"), (myMap.tilesize,myMap.tilesize)),
+	"COAL": pygame.transform.scale(pygame.image.load("images/coal.png"), (myMap.tilesize,myMap.tilesize))
+}
+
+tilemap = [[0 for w in range(myMap.width)] for h in range(myMap.height)]
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((myMap.width*myMap.tilesize, myMap.height*myMap.tilesize))
+
+for rw in range(myMap.height):
+	for cl in range(myMap.width):
+		randomNumber = random.randint(0,15)
+		if randomNumber == 0:
+			tile = "COAL"
+		elif randomNumber in [1,2]:
+			tile = "WATER"
+		elif randomNumber in range(3,8):
+			tile = "GRASS"
+		else:
+			tile = "ROCK"
+		tilemap[rw][cl] = tile
+		DISPLAYSURF.blit(textures[tilemap[rw][cl]], (cl*myMap.tilesize, rw*myMap.tilesize))
+
+while True:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+	pygame.display.update()
