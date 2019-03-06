7d82803562232dcfb17945c57b621dac9cc19d6a
Andrew McKernan <andrewmck96@gmail.com>
2019-03-06 16:17:39 -0700

    created rule_enactor.py

---
+++ game_engine/rule_enactor.py
@@ -0,0 +1,11 @@
+
+class RuleEnactor:
+	"""
+	Rule Enactor class. Takes rules from rule interpreter and assists game engine/view with implementing them.
+	"""
+	
+	def __init__(self):
+		# something
+		
+	def interpret():
+		# ...
