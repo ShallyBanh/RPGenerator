4b82e1587243f7f1f73c56fc338357c12b43d821
ThomasTetz <thomas.tetz@gmail.com>
2019-03-11 01:23:37 -0600

    EmailSender to send recovery emails
    3.3.3.4

---
+++ network/email_sender.py
@@ -0,0 +1,31 @@
+import smtplib
+class EmailSender:
+    def __init__(self, smtp_server="smtp.gmail.com", port=465,
+        sender="rpgenerator.recovery@gmail.com", passwd="Capstonerecovery"):
+
+        self.smtp_server = smtp_server
+        self.port = port
+        self.sender = sender
+        self.passwd = passwd
+        self.server = smtplib.SMTP_SSL(self.smtp_server, self.port)
+        self.server.login(self.sender, self.passwd)
+        self.recovery_body = "To: {0}\r\nFrom: " + self.sender +  "\r\nSubject: RPGenerator Account Recovery\r\n\r\nYour recovery code for account {1} is: {2}"
+
+    def send_email(self, recipient, message):
+        try:
+            self.server.sendmail(self.sender, recipient, message)
+            print ('email sent')
+            return 0
+        except Exception as e:
+            print('error sending mail: '+ str(e))
+            return -1
+  
+if __name__ == "__main__":
+    emailer = EmailSender()
+    recipient = "thomas.tetz@gmail.com"
+    username = "user1"
+    code = "12345"
+    message = emailer.recovery_body.format(recipient, username, code)
+    emailer.send_email(recipient, message)
+
+   
