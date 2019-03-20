781f7f4e3a5c99f845cef3c07bc28dec8c77be4b
Andrew McKernan <andrewmck96@gmail.com>
2019-03-20 16:41:37 -0600

    fixed within statement and dummy Entity class. completed movetowards and moveaway functions

--- game_engine/rule_enactor.py
+++ game_engine/rule_enactor.py
@@ -25,9 +25,18 @@ class Entity:
 	def __init__(self, name, type):
 		self.name = name
 		self.type = type
+		self.attributes = []
 			
 	def add_attribute(self, attribute):
+		print(self.name)
 		self.attributes.append(attribute)
+		print(self.attributes)
+		
+	def __str__(self):
+		attrs = ""
+		for a in self.attributes:
+			attrs += a.name + ":" + str(a.value) + " "
+		return "Name: " + self.name + " Type: " + self.type + attrs
 		
 class Point:
 	x = 0
@@ -48,17 +57,17 @@ class RuleEnactor:
 	
 	all_created_entities = []
 	
+	# see bottom
 	keywords = {}
-				
+	
+	# see bottom
 	operators = {}
-				
-	#spaceless_operators = ["+", "-", "/", "*", "=", "==", "<", ">"]
-				
+	
+	# key: variable name (string), value: variable value
 	variables = {}
 	
-	#####when a within statement encompasses a group, this will be filled up with a key "entity_type" and a list of all entities that match the condition of within. Every function modifying an entity will check this dict to see if they need to be targeting a group or just a single target. This dict will be emptied at the start of each new line read since a within statement does not persist longer than that.
-	# targeted_entities = {}
-	# dict_key = ''
+	# key: entity_type.ActionName, value: rule string to execute
+	relationships = {}
 	
 	current_entity_in_loop = None
 	
@@ -72,6 +81,12 @@ class RuleEnactor:
 		
 	def add_new_entity(self, entity):
 		self.all_created_entities.append(entity)
+		
+	def add_all_entities(self, line):
+		#TODO
+	
+	def add_all_relationships(self, line):
+		#TODO
 				
 	def _is_number(s):
 		try: 
@@ -90,6 +105,8 @@ class RuleEnactor:
 		print(lines)
 		#set self here
 		self.acting_entity = acting_entity
+		print("Acting Entity: ")
+		print(self.acting_entity)
 		# perform each line
 		for line in lines:
 			print("Evaluating line: " + line)
@@ -181,8 +198,14 @@ class RuleEnactor:
 			for entity in self.all_created_entities:
 				if entity.name == target_type:
 					self.target_of_action = entity
+					print("New Target :")
+					print(self.target_of_action)
 		
+	
+	def handle_on(self, line):
 		
+	
+	
 	def handle_if(self, line):
 		# handle all case
 		original_line = line
@@ -196,9 +219,14 @@ class RuleEnactor:
 			action = conditional_and_action[1].strip()
 			
 			for entity in self.all_created_entities:
+				print("all created entities: " + str(self.all_created_entities))
+				print("Setting current entitiy in loop to" + str(entity))
 				self.current_entity_in_loop = entity
-				if self.evaluate_line(self, conditional):
-					return self.evaluate_line(self, action)
+				tf = self.evaluate_line(self, conditional)
+				print("Did said entity satisfy condition?")
+				print(tf)
+				if tf:
+					self.evaluate_line(self, action)
 		else:
 			post_if = line.split('if')[1].strip()
 			print(post_if)
@@ -362,8 +390,6 @@ class RuleEnactor:
 		# item1/2 may be: a point, a target entity, the acting_entity, or referring to a general type of
 		# entities. We must determine the case.
 		# We know at least one of item1/2 is referring to a specific target (point, entity, or self), and only one could possibly be a general type.
-		#TODO: parse these out
-		# entities_of_type = []
 		item1 = None
 		item2 = None
 		words = written_rule.split()
@@ -375,6 +401,7 @@ class RuleEnactor:
 			item1 = self.target_of_action
 		elif item1_string == self.current_entity_in_loop.type:
 			item1 = self.current_entity_in_loop
+			#print("setting current entity in loop to" + str(self.current_entity_in_loop))
 		#item 2
 		if item2_string == 'self':
 			item2 = self.acting_entity
@@ -382,6 +409,7 @@ class RuleEnactor:
 			item2 = self.target_of_action
 		elif item2_string == self.current_entity_in_loop.type:
 			item2 = self.current_entity_in_loop
+			#print("setting current entity in loop to" + statuses(self.current_entity_in_loop))
 		
 		left_bracket_idx = written_rule.find('(')
 		right_bracket_idx = written_rule.find(')')
@@ -392,40 +420,6 @@ class RuleEnactor:
 		
 		within_x = False
 		within_y = False
-		# if len(entities_of_type) > 0:
-			# item_to_compare = None
-			# if item1 is not None:
-				# item_to_compare = item1
-			# else:
-				# item_to_compare = item2
-			# for entity in entities_of_type:
-				##x cases
-				# if entity == item_to_compare:
-					# continue
-				# if entity.x < item_to_compare.x:
-					# if entity.x + x_dist >= item_to_compare.x:
-						# within_x = True
-				# elif entity.x > item_to_compare.x:
-					# if entity.x - x_dist <= item_to_compare.x:
-						# within_x = True
-				# elif entity.x == item_to_compare.x:
-					# within_x = True
-				##y cases	
-				# if entity.y < item_to_compare.y:
-					# if entity.y + y_dist >= item_to_compare.y:
-						# within_y = True
-				# elif entity.y > item_to_compare.y:
-					# if entity.y - y_dist <= item_to_compare.y:
-						# within_y = True
-				# elif entity.y == item_to_compare.y:
-					# within_y = True
-				##therefore, is within? 
-				# if within_y and within_x:
-					# self.targeted_entities[self.dict_key].append(entity)
-			# if len(self.targeted_entities[self.dict_key]) > 0:
-				# return True
-			# else:
-				# return False
 		if item1 is not None and item2 is not None:
 			# x cases
 			if item1.x < item2.x:
@@ -451,17 +445,103 @@ class RuleEnactor:
 			return False
 				
 		
+	def handle_move(self, written_rule):
+		if "towards" in written_rule:
+			handle_movetowards(self, written_rule)
+		elif "away" in written_rule:
+			handle_moveaway(self, written_rule)
+		
 	def handle_moveaway(self, written_rule):
 		#TODO
 		# handle moving targets away from a given point or entity
-		print("moveaway...")
-		return
+		words = split()
+		entity_to_move = None
+		item_to_move_from = None
+		entity_to_move_string = words[1]
+		item_to_move_string = words[-1]
+		distance_to_move = int(words[2])
+		if entity_to_move_string == 'self':
+			entity_to_move = acting_entity
+		elif entity_to_move_string == 'target'
+			entity_to_move = target_of_action
+		elif entity_to_move_string == current_entity_in_loop.type:
+			entity_to_move = current_entity_in_loop
+		
+		if item_to_move_string == 'self':
+			item_to_move_from = acting_entity
+		elif item_to_move_string == 'target'
+			item_to_move_from = target_of_action
+		elif item_to_move_string == current_entity_in_loop.type:
+			item_to_move_from = current_entity_in_loop
+			
+		if entity_to_move == item_to_move_from:
+			return
+			
+		x_dist = entity_to_move.x - item_to_move_from.x
+		y_dist = entity_to_move.y - item_to_move_from.y
+		# move horizontally 
+		if abs(x_dist) >= abs(y_dist):
+			if x_dist < 0:
+				#entity to move is to the left, so move more left
+				entity_to_move.x -= distance_to_move
+			else:
+				# otherwise go right
+				entity_to_move.x += distance_to_move
+		else:
+			#move vertically
+			if y_dist < 0:
+				entity_to_move.y -= distance_to_move
+			else:
+				# otherwise go right
+				entity_to_move.y += distance_to_move
 		
 	def handle_movetowards(self, written_rule):
 		#TODO
-		# handle moving targets towards a given point or entity
-		print("movetowards...")
-		return
+		# handle moving targets away from a given point or entity
+		words = split()
+		entity_to_move = None
+		item_to_move_from = None
+		entity_to_move_string = words[1]
+		item_to_move_string = words[-1]
+		distance_to_move = int(words[2])
+		if entity_to_move_string == 'self':
+			entity_to_move = acting_entity
+		elif entity_to_move_string == 'target'
+			entity_to_move = target_of_action
+		elif entity_to_move_string == current_entity_in_loop.type:
+			entity_to_move = current_entity_in_loop
+		
+		if item_to_move_string == 'self':
+			item_to_move_from = acting_entity
+		elif item_to_move_string == 'target'
+			item_to_move_from = target_of_action
+		elif item_to_move_string == current_entity_in_loop.type:
+			item_to_move_from = current_entity_in_loop
+			
+		if entity_to_move == item_to_move_from:
+			return
+			
+		x_dist = entity_to_move.x - item_to_move_from.x
+		y_dist = entity_to_move.y - item_to_move_from.y
+		# move horizontally 
+		if abs(x_dist) >= abs(y_dist):
+			if abs(x_dist) < distance_to_move:
+					distance_to_move = abs(x_dist)
+			if x_dist < 0:
+				#entity to move is to the left, so move more left
+				entity_to_move.x += distance_to_move
+			else:
+				# otherwise go right
+				entity_to_move.x -= distance_to_move
+		else:
+			#move vertically
+			if abs(y_dist) < distance_to_move:
+					distance_to_move = abs(y_dist)
+			if y_dist < 0:
+				entity_to_move.y += distance_to_move
+			else:
+				# otherwise go right
+				entity_to_move.y -= distance_to_move
 		
 	def handle_plus(self, written_rule):
 		words = written_rule.split('+')
@@ -630,7 +710,7 @@ class RuleEnactor:
 	keywords = {"target":handle_target, "if":handle_if,
 				"increase":handle_increase,"decrease":handle_decrease, "multiply":handle_multiply,
 				"divide":handle_divide, "set":handle_set, "reduce":handle_decrease, 
-				"towards":handle_movetowards, "away":handle_moveaway}
+				"move":handle_move}
 				
 	combined_operators = {"+=":handle_plus_equals, "-=":handle_minus_equals, 
 				"*=":handle_times_equals, "/=":handle_divide_equals, "==":handle_equals,}
