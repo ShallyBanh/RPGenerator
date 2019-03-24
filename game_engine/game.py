93a329c25a4db3c0516c6fe664b93dcda3105799
ThomasTetz <thomas.tetz@gmail.com>
2019-03-24 11:49:57 -0600

    Game class outline

---
+++ game_engine/game.py
@@ -0,0 +1,76 @@
+from .map import Map
+
+class Game:
+    def __init__(self):
+        self.uniqueID = None
+        self.GM = None
+        self.name = None
+        self.players = []
+        self.entities = [] # possibly use dict instead
+        self.map = Map(50, 10, 18) # parameters?
+        self.ruleset_copy = None
+        self.assets = []
+        self.transcript = ""
+    
+    def __str__(self):
+        string = "Game:"
+        for k in self.__dict__.keys():
+            string += "\n\t{}: {}".format(k, self.__dict__[k])
+        return string
+
+    def get_uniqueID(self):
+        return self.uniqueID
+
+    def set_uniqueID(self, ID):
+        self.uniqueID = ID
+
+    def get_GM(self):
+        return self.GM
+
+    def set_GM(self, GM):
+        self.GM = GM
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_players(self):
+        return self.players
+
+    def set_players(self, players):
+        self.players = players
+
+    def get_entities(self):
+        return self.entities
+
+    def set_entities(self, entities):
+        self.uniqueentities = entities
+
+    def get_map(self):
+        return self.map
+
+    def set_map(self, map):
+        self.map = map
+
+    def get_ruleset_copy(self):
+        return self.ruleset_copy
+
+    def set_ruleset_copy(self, ruleset_copy):
+        self.ruleset_copy = ruleset_copy
+
+    def get_assets(self):
+        return self.assets
+
+    def add_asset(self, asset):
+        self.assets.append(asset)
+
+    def remove_asset(self, asset):
+        self.assets.pop(asset.get_name, None)
+
+    def get_transcript(self):
+        return self.transcript
+
+    def append_transcript(self, new_content):
+        self.transcript += "\n" + new_content
