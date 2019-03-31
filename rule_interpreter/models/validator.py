bdff5d28de556fc9c20e605f040d0290b1f31438
Andrew McKernan <andrewmck96@gmail.com>
2019-03-31 17:16:00 -0600

    added logic in the validator to maintain proper inheritance between entities
    when parent entities are updated, child entities will also be updated to match

--- rule_interpreter/models/validator.py
+++ rule_interpreter/models/validator.py
@@ -10,6 +10,7 @@ class _Validator(object):
         self._isValid = None
         self._allEntities = []
         self._allRelationships = []
+		self._hierarchy = {}
     
     def is_valid_syntax(self, target, content):
         """
@@ -38,9 +39,22 @@ class _Validator(object):
     
     def add_entity(self, entity):
         self._allEntities.append(entity)
+		self._hierarchy[entity.get_type()] = []
+		if entity.get_is_inherited_from() is not None:
+			self._hierarchy[entity.get_is_inherited_from().get_type()].append(entity)
     
     def remove_entity(self, entity):
-        self._allEntities.remove(entity)
+        if len(self._hierarchy[entity.get_type()]) < 1:
+			self._allEntities.remove(entity)
+			del self._hierarchy[entity.get_type()]
+			if entity.get_is_inherited_from() is not None:
+				self._hierarchy[entity.get_is_inherited_from().get_type()].remove(entity)
+		else:
+			pass
+			#do not remove since it is a parent of other entities, which could break things.
+			#TODO return an error code to determine whether the removal has succeeded.
+			
+		
     
     def get_entities(self):
         return self._allEntities
@@ -54,21 +68,39 @@ class _Validator(object):
     
     def update_attribute(self, entityIdx, oldAttributeIdx, attributeName, attributeValue):
         self._allEntities[entityIdx].update_attribute(oldAttributeIdx, Attribute(attributeName, attributeValue))
+		for entity in self._hierarchy[self._allEntities[entityIdx].get_type()]:
+			self._set_new_parent(entity, self._allEntities[entityIdx])
 
     def set_attribute(self, entityIdx, attributeName, attributeValue):
         self._allEntities[entityIdx].add_attribute(Attribute(attributeName, attributeValue))
+		for entity in self._hierarchy[self._allEntities[entityIdx].get_type()]:
+			self._set_new_parent(entity, self._allEntities[entityIdx])
 
     def update_action(self, entityIdx, oldActionIdx, actionName, ruleContent):
         self._allEntities[entityIdx].update_action(oldActionIdx, Action(actionName, ruleContent))
-    
+		for entity in self._hierarchy[self._allEntities[entityIdx].get_type()]:
+			self._set_new_parent(entity, self._allEntities[entityIdx])
+	
     def update_entity(self, entityIdx, newType, width, height, template, inheritedFrom):
+		old_type = self._allEntities[entityIdx].get_type()
         self._allEntities[entityIdx].set_type(newType)
+		self._hierarchy[newType] = self._hierarchy[old_type]
+		del self._hierarchy[old_type]
         self._allEntities[entityIdx].set_size(Size(width, height))
         self._allEntities[entityIdx].set_is_template(template)
         self._allEntities[entityIdx].set_is_inherited_from(inheritedFrom)
+		for entity in self._hierarchy[self._allEntities[entityIdx].get_type()]:
+			self._set_new_parent(entity, self._allEntities[entityIdx])
+		
+	def _set_new_parent(self, child, parent):
+		child.set_inherited_from(parent)
+		for entity in self._hierarchy[child.get_type()]:
+			self._set_new_parent(entity, child)
 
     def set_action(self, entityIdx, actionName, ruleContent):
         self._allEntities[entityIdx].add_action(Action(actionName, ruleContent))
+		for entity in self._hierarchy[self._allEntities[entityIdx].get_type()]:
+			self._set_new_parent(entity, self._allEntities[entityIdx])
 
     def set_entity(self, idx, entity):
         self._allEntities[idx] = entity
